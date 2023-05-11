5d6c93b0c4c91b1edae0d53ccdb811f4763fcade
Abin-98 <abinvarkey01998@gmail.com>
2023-05-11 23:04:31 +0530

    task3

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-console.log("I am new here!")
-console.log(document.title)
- 
+console.log(document)
+let titler=document.getElementById('header-title');
+titler.style.borderBottom="solid 3px #000";
+let subHead=document.getElementById("adder");
+subHead.style.fontWeight="bold";
+subHead.style.color="green";
