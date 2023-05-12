b3061867461fd60ed071ff443910e3e02aa2770b
Abin-98 <abinvarkey01998@gmail.com>
2023-05-12 19:28:08 +0530

    task5

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-console.log(document)
-let titler=document.getElementById('header-title');
+let titler=document.getElementById('main-header');
 titler.style.borderBottom="solid 3px #000";
 let subHead=document.getElementById("adder");
 subHead.style.fontWeight="bold";
@@ -10,3 +9,6 @@ items[2].style.backgroundColor="green";
 for(let i=0;i<items.length;i++){
     items[i].style.fontWeight="bold";
 }
+let newitem=document.getElementsByTagName("li");
+newitem[4].textContent="Changed name";
+newitem[4].style.color="#ffbbff";
